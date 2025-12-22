9298a80be85a65ffbb35c63bca0a4bfc6feab515
Nico Smith <web@niiccoo2.xyz>
2025-12-22 08:10:08 -0500

    Add debug

--- Vehicle/light_test.py
+++ Vehicle/light_test.py
@@ -7,6 +7,7 @@ for i in range(30):
     if i < 6:
         pixels[i] = (0, 255, 0)
     elif i > 26:
+        print(f'{i} is red')
         pixels[i] = (255, 0, 0)
     else:
         pixels[i] = (255, 255, 255)
