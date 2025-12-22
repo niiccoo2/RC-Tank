fb8d3af5f9a0c7a6886da8a62d568924674d85d4
Nico Smith <web@niiccoo2.xyz>
2025-12-22 08:09:15 -0500

    Add colors

--- Vehicle/light_test.py
+++ Vehicle/light_test.py
@@ -3,6 +3,13 @@ import neopixel #type:ignore
 import time
 pixels = neopixel.NeoPixel(board.D18, 30)
 
-pixels.fill((0, 255, 0))
+for i in range(30):
+    if i < 6:
+        pixels[i] = (0, 255, 0)
+    elif i > 26:
+        pixels[i] = (255, 0, 0)
+    else:
+        pixels[i] = (255, 255, 255)
+
 time.sleep(10)
 pixels.fill((0, 0, 0))
