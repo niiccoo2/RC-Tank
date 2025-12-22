8748addbe2b8b67eccb0d95b7beec5ae568c9766
Nico Smith <web@niiccoo2.xyz>
2025-12-22 10:10:34 -0500

    Fix math

--- Vehicle/light_class.py
+++ Vehicle/light_class.py
@@ -17,7 +17,7 @@ class Lights:
     
     def headlights_off(self):
         for i in range(30):
-            if i < 6 and i > 23:
+            if i > 6 and i < 24:
                 self.pixels[i] = (0, 0, 0)
     
     def off(self):
