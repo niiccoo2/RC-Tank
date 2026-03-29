579f9a832d001a1499edeadd1759c4a0e53d3e4f
Nico Smith <web@niiccoo2.xyz>
2026-03-29 18:06:15 -0400

    Change to print statement

--- Vehicle/test_files/compass_test.py
+++ Vehicle/test_files/compass_test.py
@@ -18,6 +18,5 @@ while True:
 	(dataReady,dataOverflow,dataSkippedForReading) = compass.status()
 	if (dataReady == 1):
 		(x,y,z,rotX,rotY,rotZ) = compass.heading()
-		sys.stdout.write("\rHeading: rotX={:3.0f}° rotY={:3.0f}° rotZ={:3.0f}° [x={:5.0f},y={:5.0f},z={:5.0f}], overflow={}, skip={}, Temp={}°C".format(rotX,rotY,rotZ,x,y,z,dataOverflow,dataSkippedForReading,compass.getTemperature()))
-		sys.stdout.flush()
+		print(f"Heading: rotX={rotX}° rotY={rotY}° rotZ={rotZ}° [x={x},y={y},z={z}], overflow={dataOverflow}, skip={dataSkippedForReading}, Temp={compass.getTemperature()}°C")
 	time.sleep(0.5)
