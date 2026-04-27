60c3a91e6976e764889b875e6348724d349fb00b
Nico Smith <web@niiccoo2.xyz>
2026-04-27 09:16:52 -0400

    Calc heading

--- Vehicle/scripts/ada_compass_test.py
+++ Vehicle/scripts/ada_compass_test.py
@@ -1,6 +1,7 @@
 import time
 import board #type:ignore
 import adafruit_qmc5883p #type:ignore
+import math
 
 i2c = board.I2C()
 
@@ -10,6 +11,10 @@ while True:
     mag_x, mag_y, mag_z = sensor.magnetic
 
     print(f"X:{mag_x:2.3f}, Y:{mag_y:2.3f}, Z:{mag_z:2.3f} G")
+
+    heading = math.atan2(mag_y, mag_x)*(180/math.pi)
+    print(heading)
+
     print("")
 
     time.sleep(1)
