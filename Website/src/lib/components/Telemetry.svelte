5f94df2286d4f94644c9471f49522d5e721f9a09
Nico Smith <web@niiccoo2.xyz>
2026-05-19 10:30:35 -0400

    WIP: GPS Telemetry

--- Website/src/lib/components/Telemetry.svelte
+++ Website/src/lib/components/Telemetry.svelte
@@ -9,35 +9,40 @@
 	} from '$lib/stores';
 </script>
 
-<div style="width: 48vw; height: 48vh; align-items: first; margin-top: 2vh;">
-	<div class="info_card px-4 py-2">
-		<div style="display: grid; grid-template-columns: 95px 1fr; text-align: left;">
-			<span>Left Speed:</span> <span>{$roundedLeftSpeed}</span>
-			<span>Right Speed:</span> <span>{$roundedRightSpeed}</span>
+<div style="width: 48vw; height: 48vh; display: flex; align-items: flex-start; margin-top: 2vh;">
+	<div style="display: flex; flex-direction: column;">
+		<div class="info_card px-4 py-2">
+			<div style="display: grid; grid-template-columns: 95px 1fr; text-align: left;">
+				<span>Left Speed:</span> <span>{$roundedLeftSpeed}</span>
+				<span>Right Speed:</span> <span>{$roundedRightSpeed}</span>
+			</div>
+		</div>
+
+		<div>
+			<p class="info_card px-4 py-2">Ping: {$ping}</p>
+		</div>
+
+		<div>
+			<p class="info_card px-4 py-2">Battery Voltage: {$voltage}v</p>
 		</div>
 	</div>
 
-	<div class="info_card px-4 py-2">
-		{#if $antiDoxx}
-			<div style="display: grid; grid-template-columns: 35px 1fr; text-align: left;">
-				<span>Lat:</span> <span>0</span>
-				<span>Lon:</span> <span>0</span>
-				<span>Alt:</span> <span>0m</span>
-			</div>
-		{:else}
-			<div style="display: grid; grid-template-columns: 35px 1fr; text-align: left;">
-				<span>Lat:</span> <span>{$gpsData.lat}</span>
-				<span>Lon:</span> <span>{$gpsData.lon}</span>
-				<span>Alt:</span> <span>{$gpsData.alt}m</span>
-			</div>
-		{/if}
-	</div>
-
-	<div>
-		<p class="info_card px-4 py-2">Ping: {$ping}</p>
-	</div>
-
-	<div>
-		<p class="info_card px-4 py-2">Battery Voltage: {$voltage}v</p>
+	<div style="display: flex; flex-direction: column;">
+		<div class="info_card px-4 py-2">
+			<strong>GPS Telemetry</strong>
+			{#if $antiDoxx}
+				<div style="display: grid; grid-template-columns: 35px 1fr; text-align: left;">
+					<span>Lat:</span> <span>0</span>
+					<span>Lon:</span> <span>0</span>
+					<span>Alt:</span> <span>0m</span>
+				</div>
+			{:else}
+				<div style="display: grid; grid-template-columns: 35px 1fr; text-align: left;">
+					<span>Lat:</span> <span>{$gpsData.lat}</span>
+					<span>Lon:</span> <span>{$gpsData.lon}</span>
+					<span>Alt:</span> <span>{$gpsData.alt}m</span>
+				</div>
+			{/if}
+		</div>
 	</div>
 </div>
