de02a695d2e9c8a342b2fd7561a0e3690dc1e738
Nico Smith <web@niiccoo2.xyz>
2026-06-23 19:58:25 -0400

    Fix log reader to use correct logic

--- Vehicle/scripts/read_log.py
+++ Vehicle/scripts/read_log.py
@@ -3,15 +3,7 @@ end_time: float = 0
 
 with open("./tank.log", "r") as f:
   for line in f:
-    if "Software started at" in line:
+    if "Time to fix" in line:
       split_line = line.split(" ")
-      start_time = float(split_line[-1]) # last line
-    elif "GPS thread started" in line:
-      split_line = line.split(" ")
-      end_time = float(split_line[-1]) # last line
+      print(split_line[-1]) # last line
     
-    if start_time != 0 and end_time != 0:
-      time_to_fix = end_time - start_time
-      start_time, end_time = 0, 0
-      print(time_to_fix)
-
